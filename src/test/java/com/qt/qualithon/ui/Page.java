e66a56c919e8829975554e8a94ba51bd7ad7c5d1
ashutoshtiwari29 <ashutosh7520@gmail.com>
2022-03-29 13:50:15 +0530

    first commit

--- src/test/java/com/qt/qualithon/ui/Page.java
+++ src/test/java/com/qt/qualithon/ui/Page.java
@@ -1,21 +1,29 @@
 package com.qt.qualithon.ui;
 
+
+
 import com.qt.qualithon.TestSession;
 
+
+
 /**
- * Abtract base page to hold all default and design implementations of any generic web page
- **/
+* Abtract base page to hold all default and design implementations of any generic web page
+**/
 public abstract class Page {
-    
-    public TestSession testSession;
 
-    public Page(TestSession testSession){
-        this.testSession = testSession;
-    }
+public TestSession testSession;
 
-    public void sleep(Integer seconds){
-        try{
-            Thread.sleep(seconds*1000);
-        }catch(Exception e){}
-    }
+
+
+public Page(TestSession testSession){
+this.testSession = testSession;
+}
+
+
+
+public void sleep(Integer seconds){
+try{
+Thread.sleep(seconds*1000);
+}catch(Exception e){}
+}
 }
